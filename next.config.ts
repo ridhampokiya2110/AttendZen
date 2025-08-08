18ea890928baaa05efb79281b50483688906181a
ridhampokiya2110 <ridhampokiya10@gmail.com>
2025-08-08 12:22:41 +0530

    ./

--- next.config.ts
+++ next.config.ts
@@ -2,24 +2,14 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  distDir: 'out',
-  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
   },
 };
 
